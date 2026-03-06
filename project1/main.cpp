f5ff6b1dc0ad6baa3571ab6f54ded2e6d85b79e4
otar-gordeladze <vilfang17@gmail.com>
2026-03-06 11:59:31 +0100

    testing

---
+++ project1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello project111" << endl;
+    return 0;
+}
---
+++ project2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello project2222" << endl;
+    return 0;
+}
